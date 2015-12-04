3f4740a439733040766427a683c0bf5d029dcf53
Matt hill <matt@smackagency.com>
2015-12-04 16:52:28 +0000

    Finishing header

--- wp-content/themes/abdulnasser/header.php
+++ wp-content/themes/abdulnasser/header.php
@@ -35,16 +35,16 @@
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							Abdulnasser Gharem
 						</a>
+					</div>
+
+					<div class="gharem-studio">
+
 					</div>
 					<!-- /logo -->
 
 					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
 
 			</header>
 			<!-- /header -->
